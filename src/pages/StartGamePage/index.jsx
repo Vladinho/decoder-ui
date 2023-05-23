f77dcee9cfbdc4100b7dd5c74ddb2ef05e677562
Vladislav Repkin <vladislav_repkin@epam.com>
2023-05-23 21:42:52 +0300

    init

--- src/pages/StartGamePage/index.jsx
+++ src/pages/StartGamePage/index.jsx
@@ -28,27 +28,27 @@ const StartGame = () => {
     return <Layout>
         <h1>Room ID:</h1>
         <h3 className={'mb-4'}>{state.roomId}</h3>
-        <button className={classNames(['btn', ' btn-primary', 'mb-2', 'w-100'])} onClick={async () => {
-            server.getRoom();
-        }}>See team members</button>
-        <button className={classNames(['btn', ' btn-primary', 'mb-2', 'w-100'])} disabled={state.users.length < 4} onClick={async () => {
-            const room = await api.createTeams(state.roomId);
-            const { data: { team_1, team_2 } } = room;
-            dispatch(setState({team_1, team_2}));
-        }}>Create teams</button>
-
-        <button className={classNames(['btn', ' btn-primary', 'mb-2', 'w-100'])} disabled={!state.team_1.length || !state.team_2.length} onClick={async () => {
-            const round = await api.nextRound(state.roomId, state.gameId, 0);
-            dispatch(setState({ round: round.data.round }));
-            navigate('/game');
-        }}>Start Game</button>
-
-        {!!state.team_1.length && !!state.team_2.length && <button onClick={() => setIsDragMode(true)} className={'btn btn-primary w-100 mb-2'}>Change teams
-            order</button>}
 
         { isDragMode ?
             <TeamsDragAndDrop onSave={() => setIsDragMode(false)} onCancel={() => setIsDragMode(false)} /> :
             <>
+                <button className={classNames(['btn', ' btn-primary', 'mb-2', 'w-100'])} onClick={async () => {
+                    server.getRoom();
+                }}>See team members</button>
+                <button className={classNames(['btn', ' btn-primary', 'mb-2', 'w-100'])} disabled={state.users.length < 4} onClick={async () => {
+                    const room = await api.createTeams(state.roomId);
+                    const { data: { team_1, team_2 } } = room;
+                    dispatch(setState({team_1, team_2}));
+                }}>Create teams</button>
+
+                <button className={classNames(['btn', ' btn-primary', 'mb-2', 'w-100'])} disabled={!state.team_1.length || !state.team_2.length} onClick={async () => {
+                    const round = await api.nextRound(state.roomId, state.gameId, 0);
+                    dispatch(setState({ round: round.data.round }));
+                    navigate('/game');
+                }}>Start Game</button>
+
+                {!!state.team_1.length && !!state.team_2.length && <button onClick={() => setIsDragMode(true)} className={'btn btn-primary w-100 mb-2'}>Change teams
+                    order</button>}
             {
                 !!state.team_1.length && !!state.team_2.length ? <>
                     <h2 className={'mt-4'}>Team 1:</h2>
