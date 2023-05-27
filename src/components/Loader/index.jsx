30c1e59dd284ea895379d6cac3f867e65a4a1a5e
Vladislav Repkin <vladislav_repkin@epam.com>
2023-05-27 13:06:06 +0300

    new loader and font

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,30 @@
 import css from './styles.module.scss';
 const Loader = () => {
-    return <div className={css.container}><div className="spinner-border" role="status">
-    </div></div>
+    return <div className={css.container}>
+        <div className={css.line}></div>
+        <div className={css.loader}>
+            <div className={`${css.oneLoader} first`}>
+                <div className={`${css.vLine} ${css.firstLine}`}></div>
+                <div className={`${css.circle} ${css.firstCircle}`}></div>
+            </div>
+            <div className={`${css.oneLoader}`}>
+                <div className={`${css.vLine} ${css.middleLine}`}></div>
+                <div className={`${css.circle} ${css.middleCircle}`}></div>
+            </div>
+            <div className={css.oneLoader}>
+                <div className={`${css.vLine} ${css.middleLine}`}></div>
+                <div className={`${css.circle} ${css.middleCircle}`}></div>
+            </div>
+            <div className={css.oneLoader}>
+                <div className={`${css.vLine} ${css.middleLine}`}></div>
+                <div className={`${css.circle} ${css.middleCircle}`}></div>
+            </div>
+            <div className={`${css.oneLoader} last`}>
+                <div className={`${css.vLine} ${css.lastLine}`}></div>
+                <div className={`${css.circle} ${css.lastCircle}`}></div>
+            </div>
+        </div>
+    </div>
 }
 
 export default Loader;
