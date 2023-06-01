98219c91203d2ea2d1c4a5987d24bf54dc9648b8
Vladislav Repkin <vladislav_repkin@epam.com>
2023-06-01 13:46:54 +0300

    banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,7 +1,9 @@
 import css from './styles.module.scss';
+import bg from '../../assets/think_2.gif'
 
 const Banner = () => {
-    return <div className={`${css.container} mt-4 mb-4 alert alert-warning`}  role="alert">
+    return <div className={`${css.container} mb-4 alert alert-warning`}  role="alert">
+        <img src={bg} className={css.thinking}/>
         <p className={'m-0'}>Other players are thinking</p>
         <span className={css.loader}></span>
     </div>
