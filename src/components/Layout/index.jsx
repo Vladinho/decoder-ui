197d3fcbfcbf96b4481c7019a21e411bc1ab74e3
Vladislav Repkin <vladislav_repkin@epam.com>
2023-06-01 11:33:59 +0300

    header

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -18,69 +18,75 @@ const Layout = ({ children, cx = [] }) => {
     const [isTeamsVisible, setIsTeamsVisible] = useState(false);
     const showModal = useModal();
     const curAnswers = state.answers.filter(i => i.round === state.round);
-    return <div className={classNames(['container-md', 'p-3', ...cx])}>
+    return <>
         {state.me && <>
-            <div className={`d-flex justify-content-between position-relative align-items-center flex-wrap p-3 shadow-sm ${css.header} mb-2`}>
-                <h2>{state.me}</h2> {!!state.round && <span className="badge bg-secondary">round {state.round}</span>}
-                <button type="button" className="btn btn-primary" onClick={() => setIsTeamsVisible((s) => !s)}>{isTeamsVisible ? 'Hide' : 'Show'} teams</button>
-                <button className="btn btn-primary" onClick={() => server.reloadData()}>
-                    <img src={refreshIcon} alt={'refresh'}/>
-                </button>
-                {isTeamsVisible && <div className={classNames([css.container, 'shadow', 'p-3', 'mb-5', 'bg-white', 'rounded', 'animate__animated animate__bounceInDown', {[css.isDnd]: state.isDndInProgress} ])}>
-                    <div className={`${css.teamsHeader} shadow`}>
-                        <button type="button" className={classNames(['btn', 'btn-light', css.close])} aria-label="Close" onClick={() => setIsTeamsVisible(false)}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <h3>Teams</h3>
-                    </div>
-                    { isDragMode ? <TeamsDragAndDrop onSave={() => setIsDragMode(false)} onCancel={() => setIsDragMode(false)} /> : <>
-                        <h3>Your team:</h3>
-                        <ul className={classNames(["list-group", 'mb-3'])}>
-                            {myTeam.map(i => <li
-                                key={i}
-                                className={classNames("list-group-item", {border: i === curPlayer, 'bg-light': i === curPlayer})}
-                            >
-                                <h6>{i}
-                                    {state.mainUser === i && ' (admin)'}
-                                    {!!curAnswers?.length &&
-                                        answers.length === state.round &&
-                                        curAnswers.every(a => a.user === i || a[`team_${state.team_1.some(u => u === i) ? 1 : 2}_agree`]?.some(j => j === i)) &&
-                                        <img src={done} className={css.done} alt={'done'}/>}
-                                    {i === curPlayer && !curAnswers.some(i => i.round === state.round && i.user === curPlayer) && <img src={think} className={css.think} alt={'think'}/>}
-                                </h6>
-                            </li>)}
-                            {!state.team_1?.length && <li className="list-group-item">No users</li>}
-                        </ul>
-                        <h3>Opponent`s team:</h3>
-                        <ul className={classNames(["list-group"])}>
-                            {opponentTeam.map(i => <li
-                                key={i}
-                                className={classNames("list-group-item", {border: i === opponentCurPlayer, 'bg-light': i === opponentCurPlayer})}
-                            >
-                                <h6>
-                                    {i}
-                                    {state.mainUser === i && ' (admin)'}
-                                    {!!curAnswers?.length &&
-                                        opponentAnswers.length === state.round &&
-                                        curAnswers.every(a => a.user === i || a[`team_${state.team_1.some(u => u === i) ? 1 : 2}_agree`]?.some(j => j === i)) &&
-                                        <img src={done} className={css.done} alt={'done'}/>}
-                                    {i === opponentCurPlayer && !curAnswers.some(i => i.round === state.round && i.user === opponentCurPlayer) && <img src={think} className={css.think} alt={'think'}/>}
-                                </h6>
-                            </li>)}
-                            {!state.team_2?.length && <li className="list-group-item">No users</li>}
-                            { state.mainUser === state.me && <button type="button" className="btn btn-primary mt-4 w-100" onClick={() => showModal( async () => {
-                                await server.reset();
-                                setIsTeamsVisible(false);
-                            })}>Restart the game</button>}
-                            { state.mainUser === state.me && <button type="button" className="btn btn-primary mt-2 w-100" onClick={() => showModal(() => server.mixTeams())}>Mix the teams</button>}
-                            { state.mainUser === state.me && <button type="button" className="btn btn-primary mt-2 w-100" onClick={() => setIsDragMode(true)}>Change the order</button>}
-                        </ul>
-                    </>}
-                </div>}
+            <div className={`p-3 shadow-sm ${css.header} mb-2`}>
+
+                <div className={'container-md d-flex justify-content-between align-items-center flex-wrap'}>
+                    <h2>{state.me}</h2> {!!state.round && <span className="badge bg-secondary">round {state.round}</span>}
+                    <button type="button" className="btn btn-primary" onClick={() => setIsTeamsVisible((s) => !s)}>{isTeamsVisible ? 'Hide' : 'Show'} teams</button>
+                    <button className="btn btn-primary" onClick={() => server.reloadData()}>
+                        <img src={refreshIcon} alt={'refresh'}/>
+                    </button>
+                    {isTeamsVisible && <div className={classNames([css.container, 'shadow', 'p-3', 'mb-5', 'bg-white', 'rounded', 'animate__animated animate__bounceInDown', {[css.isDnd]: state.isDndInProgress} ])}>
+                        <div className={`${css.teamsHeader} shadow`}>
+                            <button type="button" className={classNames(['btn', 'btn-light', css.close])} aria-label="Close" onClick={() => setIsTeamsVisible(false)}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <h3>Teams</h3>
+                        </div>
+                        { isDragMode ? <TeamsDragAndDrop onSave={() => setIsDragMode(false)} onCancel={() => setIsDragMode(false)} /> : <>
+                            <h3>Your team:</h3>
+                            <ul className={classNames(["list-group", 'mb-3'])}>
+                                {myTeam.map(i => <li
+                                    key={i}
+                                    className={classNames("list-group-item", {border: i === curPlayer, 'bg-light': i === curPlayer})}
+                                >
+                                    <h6>{i}
+                                        {state.mainUser === i && ' (admin)'}
+                                        {!!curAnswers?.length &&
+                                            answers.length === state.round &&
+                                            curAnswers.every(a => a.user === i || a[`team_${state.team_1.some(u => u === i) ? 1 : 2}_agree`]?.some(j => j === i)) &&
+                                            <img src={done} className={css.done} alt={'done'}/>}
+                                        {i === curPlayer && !curAnswers.some(i => i.round === state.round && i.user === curPlayer) && <img src={think} className={css.think} alt={'think'}/>}
+                                    </h6>
+                                </li>)}
+                                {!state.team_1?.length && <li className="list-group-item">No users</li>}
+                            </ul>
+                            <h3>Opponent`s team:</h3>
+                            <ul className={classNames(["list-group"])}>
+                                {opponentTeam.map(i => <li
+                                    key={i}
+                                    className={classNames("list-group-item", {border: i === opponentCurPlayer, 'bg-light': i === opponentCurPlayer})}
+                                >
+                                    <h6>
+                                        {i}
+                                        {state.mainUser === i && ' (admin)'}
+                                        {!!curAnswers?.length &&
+                                            opponentAnswers.length === state.round &&
+                                            curAnswers.every(a => a.user === i || a[`team_${state.team_1.some(u => u === i) ? 1 : 2}_agree`]?.some(j => j === i)) &&
+                                            <img src={done} className={css.done} alt={'done'}/>}
+                                        {i === opponentCurPlayer && !curAnswers.some(i => i.round === state.round && i.user === opponentCurPlayer) && <img src={think} className={css.think} alt={'think'}/>}
+                                    </h6>
+                                </li>)}
+                                {!state.team_2?.length && <li className="list-group-item">No users</li>}
+                                { state.mainUser === state.me && <button type="button" className="btn btn-primary mt-4 w-100" onClick={() => showModal( async () => {
+                                    await server.reset();
+                                    setIsTeamsVisible(false);
+                                })}>Restart the game</button>}
+                                { state.mainUser === state.me && <button type="button" className="btn btn-primary mt-2 w-100" onClick={() => showModal(() => server.mixTeams())}>Mix the teams</button>}
+                                { state.mainUser === state.me && <button type="button" className="btn btn-primary mt-2 w-100" onClick={() => setIsDragMode(true)}>Change the order</button>}
+                            </ul>
+                        </>}
+                    </div>}
+                </div>
             </div>
         </>}
-        {children}
-    </div>
+        <div className={classNames(['container-md', 'p-3', ...cx])}>
+            {children}
+        </div>
+    </>
+
 }
 
 export default Layout;
